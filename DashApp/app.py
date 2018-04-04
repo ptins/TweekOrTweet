6c971f0cf592e97a73f13e6ca0175d470195e7a1
ptins <ptinsley@nd.edu>
2018-04-04 19:35:51 -0400

    clean up and dashapp

--- DashApp/app.py
+++ DashApp/app.py
@@ -1,58 +1,142 @@
+import numpy as np
 import pandas as pd
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-app = dash.Dash()
+from sklearn import preprocessing
 
-df = pd.read_csv('user_tweets_FROM.csv')
-print(df.head())
+## start app
+
+app = dash.Dash(__name__)
+server = app.server
+
+## load data
+
+people = pd.read_csv('people_list.csv') # for later
+df = pd.read_csv('user_tweets_from.csv', header=None)
 df.columns = ['tweet_id','created_at','favorite_count','retweet_count','followers_count','screen_name','industry']
+df.set_index('tweet_id', inplace=True)
+
+# feature processing
+df['rfr'] = (df['favorite_count']+df['retweet_count'])/df['followers_count']
+
+# normalization
+tmp = df.drop(['created_at','screen_name','industry'],axis=1)
+normalized = (tmp-tmp.min())/(tmp.max()-tmp.min())
+normalized['created_at']= df['created_at']
+normalized['screen_name']= df['screen_name']
+normalized['industry']= df['industry']
+df = normalized
+
+## split data into industry for traces
 celeb_df = df[df['industry']=='celebrity']
 athlete_df = df[df['industry']=='athlete']
 musician_df = df[df['industry']=='musician']
 
+# take care of averages
+df_ave = df.groupby('screen_name').mean()
+df_ave = pd.merge(df_ave, people, left_index=True, right_on='screen_name')
+
+celeb_df_ave = df_ave[df_ave['industry']=='celebrity']
+athlete_df_ave = df_ave[df_ave['industry']=='athlete']
+musician_df_ave = df_ave[df_ave['industry']=='musician']
+
 trace1 = dict(
         type='scatterternary',
         a= celeb_df['retweet_count'],
-        b= celeb_df['retweet_count'],
+        b= celeb_df['rfr'],
         c= celeb_df['favorite_count'],
         text= celeb_df['screen_name'],
         mode='markers',
-        marker={
-            'symbol': 100,
+        opacity= .5,
+        marker= {
+            'symbol': 'o',
             'color': 'green',
-            'size': 10
-        }
+            'size': 14
+        },
+        name='Celebrities'
 )
 
 trace2 = dict(
         type='scatterternary',
         a= athlete_df['retweet_count'],
-        b= athlete_df['retweet_count'],
+        b= athlete_df['rfr'],
         c= athlete_df['favorite_count'],
         text= athlete_df['screen_name'],
         mode='markers',
-        marker={
-            'symbol': 100,
+        opacity= .5,
+        marker= {
+            'symbol': athlete_df['screen_name'],
             'color': 'red',
-            'size': 10
-        }
+            'size': 14
+        },
+        name='Athletes'
 )
 
 trace3 = dict(
         type='scatterternary',
         a= musician_df['retweet_count'],
-        b= musician_df['retweet_count'],
+        b= musician_df['rfr'],
         c= musician_df['favorite_count'],
         text= musician_df['screen_name'],
         mode='markers',
-        marker={
-            'symbol': 100,
+        opacity= .5,
+        marker= {
+            'symbol': musician_df['screen_name'],
             'color': 'blue',
-            'size': 10
-        }
+            'size': 14
+        },
+        name='Musicians'
+)
+
+trace4 = dict(
+        type='scatterternary',
+        a= celeb_df_ave['retweet_count'],
+        b= celeb_df_ave['rfr'],
+        c= celeb_df_ave['favorite_count'],
+        text= celeb_df_ave['screen_name'],
+        mode='markers',
+        opacity= .5,
+        marker= {
+            'symbol': 'o',
+            'color': 'green',
+            'size': 14
+        },
+        name='Celebrities'
+)
+
+trace5 = dict(
+        type='scatterternary',
+        a= athlete_df_ave['retweet_count'],
+        b= athlete_df_ave['rfr'],
+        c= athlete_df_ave['favorite_count'],
+        text= athlete_df_ave['screen_name'],
+        mode='markers',
+        opacity= .5,
+        marker= {
+            'symbol': 'o',
+            'color': 'red',
+            'size': 14
+        },
+        name='Athletes'
+)
+
+trace6 = dict(
+        type='scatterternary',
+        a= musician_df_ave['retweet_count'],
+        b= musician_df_ave['rfr'],
+        c= musician_df_ave['favorite_count'],
+        text= musician_df_ave['screen_name'],
+        mode='markers',
+        opacity= .5,
+        marker= {
+            'symbol': 'o',
+            'color': 'blue',
+            'size': 14
+        },
+        name='Musicians'
 )
 
 app.layout = html.Div(children=[
@@ -60,42 +144,91 @@ app.layout = html.Div(children=[
     html.Div(children=[
 
         html.H1(children='Twitter Persona Likability'),
-        html.P(children='Judge Individuals\' Likability Based on Tweeting Habits'),
+        html.P(children='Judge Individuals\' Likability Based on Tweeting Habits', style={'font-style': 'italic'}),
 
-    ]),
-
-    html.Div(children=[
-
-        html.H3('Select Industry'),
-        dcc.Dropdown(
-            options=[
-                {'label': 'Celebrities', 'value': 'celebrity'},
-                {'label': 'Athletes', 'value': 'athlete'},
-                {'label': 'Musicians', 'value': 'musician'}
-            ],
-            value=['celebrity', 'athlete', 'musician'],
-            multi=True
-            )
-        ]
+    ],
+    style = {
+            'margin':'auto',
+            'textAlign': 'center',
+    }
     ),
 
+    ## industry selection
+    html.Div(children=[
+
+        html.H2('Select Metric', style={'textAlign': 'center'}),
+        dcc.Dropdown(
+            id='metric',
+            options=[
+                {'label': 'Reactions/Followers', 'value': 'rfr'},
+                {'label': 'User Tweet Polarity', 'value': 'sentiment'}
+            ],
+            value='rfr'
+            ),
+        ],
+        style = {
+                'width':'50%',
+                'margin':'auto',
+                'textAlign': 'center',
+                'marginBottom': 20,
+        }
+    ),
+
+    html.H2('TwitTernary Plots', style={'textAlign': 'center'}),
 
     dcc.Graph(
         id='example-graph',
+        style={
+            'width': '50%',
+            'display': 'inline-block'
+        },
         figure={
-            # 'data': [trace1],
             'data': [trace1, trace2, trace3],
             'layout': {
-                'title': 'Ternary Scatter Plot',
                 'ternary': {
                     'sum': 1,
-                    'aaxis': {'title': 'X', 'min': 0.01, 'linewidth':2, 'ticks':'outside' },
-                    'baxis': {'title': 'W', 'min': 0.01, 'linewidth':2, 'ticks':'outside' },
-                    'caxis': {'title': 'S', 'min': 0.01, 'linewidth':2, 'ticks':'outside' }
+                    'aaxis': {'title': 'Retweet Count', 'min': 0.01, 'linewidth':2, 'ticks':'outside' },
+                    'baxis': {'title': 'rfr', 'min': 0.01, 'linewidth':2, 'ticks':'outside' },
+                    'caxis': {'title': 'Favorite Count', 'min': 0.01, 'linewidth':2, 'ticks':'outside' }
                 },
+                'annotations': [{
+                    'showarrow': False,
+                    'text': 'Individual Tweets',
+                    'x': 0.5,
+                    'y': 1.3,
+                    'font': { 'size': 25 }
+                }]
+            }
+        }
+    ),
+
+    dcc.Graph(
+        id='example-graph-2',
+        style={
+            'width': '50%',
+            'display': 'inline-block'
+        },
+        figure={
+            'data': [trace4, trace5, trace6],
+            'layout': {
+                'ternary': {
+                    'sum': 1,
+                    'aaxis': {'title': 'Retweet Count', 'min': 0.01, 'linewidth':2, 'ticks':'outside' },
+                    'baxis': {'title': 'rfr', 'min': 0.01, 'linewidth':2, 'ticks':'outside' },
+                    'caxis': {'title': 'Favorite Count', 'min': 0.01, 'linewidth':2, 'ticks':'outside' }
+                },
+                'annotations': [{
+                    'showarrow': False,
+                    'text': 'Average Across Tweets',
+                    'x': 0.5,
+                    'y': 1.3,
+                    'font': { 'size': 25 }
+                }]
             }
         }
     )
+
+
 
 ])
 
